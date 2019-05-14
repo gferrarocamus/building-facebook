1033ff6b256496884667c5b8cc221d44c725535e
Giuliana Ferraro <giulianaferrarocamus@gmail.com>
2019-05-14 18:39:39 +0200

    fix Facebook login issue

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -4,6 +4,11 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
     # You need to implement the method below in your model (e.g. app/models/user.rb)
     @user = User.from_omniauth(request.env["omniauth.auth"])
+    
+    if request.env["omniauth.auth"].info.email.blank?
+      redirect_to "/users/auth/facebook?auth_type=rerequest&scope=email"
+      return # be sure to include an return if there is code after this otherwise it will be executed
+    end
 
     if @user.persisted?
       sign_in_and_redirect @user, event: :authentication #this will throw if @user is not activated
--- config/initializers/omniauth.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-OmniAuth.config.logger = Rails.logger
-
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET']
-end
