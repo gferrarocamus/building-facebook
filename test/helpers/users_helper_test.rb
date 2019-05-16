2c7c972fe3c04061c9fc55ee0010ecf85777b9d6
Giuliana Ferraro <giulianaferrarocamus@gmail.com>
2019-05-16 17:41:01 +0200

    update forms

--- test/helpers/users_helper_test.rb
+++ test/helpers/users_helper_test.rb
@@ -1,13 +1,17 @@
 require 'test_helper'
 
 class UsersHelperTest < ActionView::TestCase
-  # test "request_sent? returns true if it exists, false if it does not" do
-  #   sender = users(:user_1)
-  #   receiver = users(:user_2)
-  #   sign_in(sender)
-  #   assert request_sent?(receiver.id)
-  #   assert_not request_sent?(sender.id)
-  # end
+  def current_user
+    users(:user_1)
+  end
+
+  test "request_sent? returns true if it exists, false if it does not" do
+    sender = users(:user_1)
+    receiver = users(:user_2)
+    sign_in(sender)
+    assert request_sent?(receiver.id)
+    assert_not request_sent?(sender.id)
+  end
 
   # test "returns the correct request" do
 
